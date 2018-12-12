fe480ea75bddbb40760eb1c7814310819deebf37
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2018-12-11 20:01:48 -0500

    Create Removing_elements.rb

---
+++ Ruby/Codewars/Removing_elements.rb
@@ -0,0 +1,11 @@
+# Take an array and remove every second element out of that array. Always keep the first element and start removing with the next element.
+
+# Example:
+
+# my_arr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...]
+# None of the arrays will be empty, so you don't have to worry about that!
+
+
+def remove_every_other(arr)
+  arr.select.with_index { | word, index | index % 2 == 0 }
+end
