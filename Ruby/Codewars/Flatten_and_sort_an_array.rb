10728321e4def6972f375d409a5bf8865ea65e7d
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2018-12-14 21:22:10 -0500

    Create Flatten_and_sort_an_array.rb

---
+++ Ruby/Codewars/Flatten_and_sort_an_array.rb
@@ -0,0 +1,23 @@
+# Given a two-dimensional array of integers, return the flattened version of the array with 
+# all the integers in the sorted (ascending) order.
+
+# Example:
+# Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+
+
+def flatten_and_sort(array)
+  new_array = []
+  array.each do |smaller_array| 
+    smaller_array.each do |num| 
+      new_array.push(num)
+      end
+    end
+  new_array.sort
+end
+
+
+# OR:
+# def flatten_and_sort(array)
+#   array.flatten.sort
+# end
