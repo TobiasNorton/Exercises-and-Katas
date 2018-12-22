8ce3c2b31b3299301b4587789c41dc5d8f3687e2
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2018-12-22 14:21:41 -0500

    Create Reverse_list_order.js

---
+++ JavaScript/Codewars/Reverse_list_order.js
@@ -0,0 +1,18 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples
+// reverseList([1,2,3,4]) == [4,3,2,1]
+// reverseList([3,1,5,4]) == [4,5,1,3]
+
+const reverseList = list => {
+  let newArray = []
+  for (let i = list.length - 1; i >= 0; i -= 1) {
+    newArray.push(list[i])
+  }
+  return newArray
+}
+
+
+
+// Or you could just do:
+// const reverseList = list => list.reverse()
