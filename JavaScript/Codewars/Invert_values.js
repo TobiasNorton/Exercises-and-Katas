14f87de1e7324be70f5d8b5e062a0d8fb84dc408
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2019-01-02 22:05:56 -0500

    Update Invert_values.js

--- JavaScript/Codewars/Invert_values.js
+++ JavaScript/Codewars/Invert_values.js
@@ -1,10 +1,10 @@
-Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, 
-and the negatives become positives.
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, 
+// and the negatives become positives.
 
-invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
-invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
-invert([]) == []
-You can assume that all values are integers.
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers.
 
 
 function invert(array) {
@@ -17,16 +17,16 @@ function invert(array) {
    });
 }
 
-Or, without an iterator:
+// Or, without an iterator:
 
-const invert = (array) => {
-  let invertedNumbers = []
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 0) {
-      invertedNumbers.push(0)
-    } else {
-      invertedNumbers.push(array[i] * -1)
-    }
-  }
-  return invertedNumbers
-}
+// const invert = (array) => {
+//   let invertedNumbers = []
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 0) {
+//       invertedNumbers.push(0)
+//     } else {
+//       invertedNumbers.push(array[i] * -1)
+//     }
+//   }
+//   return invertedNumbers
+// }
