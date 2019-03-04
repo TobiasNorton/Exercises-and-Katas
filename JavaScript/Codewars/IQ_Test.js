0b74409fd9bab36b773489fbb59e940f7af757ae
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2019-03-04 00:22:37 -0500

    Update IQ_Test.js

--- JavaScript/Codewars/IQ_Test.js
+++ JavaScript/Codewars/IQ_Test.js
@@ -1,3 +1,34 @@
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one 
+// of the given numbers differs from the others. Bob observed that one number usually differs from 
+// the others in evenness. Help Bob — to check his answers, he needs a program that among the given 
+// numbers finds one that is different in evenness, and return a position of this number.
+
+// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the 
+// elements start from 1 (not 0)
+
+// ##Examples :
+
+// iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+// iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+
+
+
+// My logic:
+// Split the string
+// For each number, check it's remainder when divided by 2
+// If it's remainder is 0, put in even array
+// If it's remainder is 1, put in odd array
+
+// If the even array has a length of 1, it's unique, 
+// so return the index of the character in the original array that matches the 
+// item in the even array + 1
+
+// Otherwise, return the index of the character in the original array that matches the 
+// item in the odd array + 1
+
+
+
 const iqTest = (numbers) => {
   let numbersArray = numbers.split(' ')
   let even = []
@@ -16,15 +47,5 @@ const iqTest = (numbers) => {
   }
 }
 
-// Split the string
-// For each number, check it's remainder when divided by 2
-// If it's remainder is 0, put in even array
-// If it's remainder is 1, put in odd array
 
-// If the even array has a length of 1, it's unique, 
-// so return the index of the character in the original array that matches the 
-// item in the even array + 1
-
-// Otherwise, return the index of the character in the original array that matches the 
-// item in the odd array + 1
 
