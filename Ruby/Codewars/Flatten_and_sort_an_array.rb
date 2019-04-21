9c19faf4aac1299125c8ab7b72ed49a198b4c38c
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2019-04-21 14:49:06 -0400

    Update Flatten_and_sort_an_array.rb

--- Ruby/Codewars/Flatten_and_sort_an_array.rb
+++ Ruby/Codewars/Flatten_and_sort_an_array.rb
@@ -5,19 +5,19 @@
 # Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
 
-
-def flatten_and_sort(array)
-  new_array = []
-  array.each do |smaller_array| 
-    smaller_array.each do |num| 
-      new_array.push(num)
-      end
-    end
-  new_array.sort
-end
-
-
-# OR:
+# Old way.
 # def flatten_and_sort(array)
-#   array.flatten.sort
+#   new_array = []
+#   array.each do |smaller_array| 
+#     smaller_array.each do |num| 
+#       new_array.push(num)
+#       end
+#     end
+#   new_array.sort
 # end
+
+
+# Better way. I love Ruby:
+def flatten_and_sort(array)
+  array.flatten.sort
+end
