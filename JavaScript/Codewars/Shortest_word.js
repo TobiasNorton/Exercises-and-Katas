ad91530e6d08bacb835e53eea5e04304af6f9b7e
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2020-01-14 21:44:58 -0500

    Update Shortest_word.js

--- JavaScript/Codewars/Shortest_word.js
+++ JavaScript/Codewars/Shortest_word.js
@@ -1,20 +1,29 @@
-// const findShortA = s => {
-//   const words = s.split(' ')
-//   const lengths = words.map(word => {
-//     return word.length
-//   })
-//   const sortedLengths = lengths.sort((a, b) => a - b)
-//   return sortedLengths[0]
-// }
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
 
-// const findShortB = s => {
-//   const words = s.split(' ')
-//   const lengths = words.map(word => {
-//     return word.length
-//   })
-//   return Math.min(...lengths)
-// }
 
+// Solution 1
+const findShortA = s => {
+  const words = s.split(' ')
+  const lengths = words.map(word => {
+    return word.length
+  })
+  const sortedLengths = lengths.sort((a, b) => a - b)
+  return sortedLengths[0]
+}
+
+
+// Solution 2
+const findShortB = s => {
+  const words = s.split(' ')
+  const lengths = words.map(word => {
+    return word.length
+  })
+  return Math.min(...lengths)
+}
+
+
+// Solution 3
 const findShortC = s => {
   const words = s.split(' ')
   const lengths = words.map(word => {
