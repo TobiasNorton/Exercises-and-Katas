1330feaa7279da8d59510062b5e7066d051651fe
Tobias Norton <44213270+TobiasNorton@users.noreply.github.com>
2020-12-09 16:21:05 -0500

    Create Simple_string_indices.js

---
+++ JavaScript/Codewars/Simple_string_indices.js
@@ -0,0 +1,34 @@
+// In this Kata, you will be given a string with brackets and an index of an opening bracket and 
+// your task will be to return the index of the matching closing bracket. Both the input and returned index 
+// are 0-based except in Fortran where it is 1-based. An opening brace will always have a closing brace. 
+// Return -1 if there is no answer (in Haskell, return Nothing; in Fortran, return 0; in Go, return an error)
+
+// Examples
+// solve("((1)23(45))(aB)", 0) = 10 // the opening brace at index 0 matches the closing brace at index 10
+// solve("((1)23(45))(aB)", 1) = 3 
+// solve("((1)23(45))(aB)", 2) = -1 // there is no opening bracket at index 2, so return -1
+// solve("((1)23(45))(aB)", 6) = 9
+// solve("((1)23(45))(aB)", 11) = 14
+// solve("((>)|?(*'))(yZ)", 11) = 14
+// Input will consist of letters, numbers and special characters, but no spaces. The only brackets will be ( and ).
+
+
+function solve(str,idx){
+  if (str[idx] !== '(') {
+    return -1 
+  }
+  const characters = [...str]
+  let position = 0
+  
+  for (let i = idx; i < characters.length; i++) {
+    if (characters[i] === '(') {
+      position++
+    } else if (characters[i] === ')') {
+      position--
+    }
+    if (position === 0) {
+      return i
+    }
+  }
+  
+}
